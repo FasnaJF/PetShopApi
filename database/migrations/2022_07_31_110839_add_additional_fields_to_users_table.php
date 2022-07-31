42d6958e8b5f93aef3c5e2edcdf528719badd4f7
jffasna <fasna@rhino-partners.com>
2022-07-31 17:09:02 +0530

    migrations added

---
+++ database/migrations/2022_07_31_110839_add_additional_fields_to_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->char('uuid', 36);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')->default(0);
+            $table->char('avatar', 36)->nullable();
+            $table->string('address');
+            $table->string('phone_number');
+            $table->boolean('is_marketing')->default(0);
+            $table->timestamp('last_login_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('is_admin');
+            $table->dropColumn('avatar');
+            $table->dropColumn('address');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('is_marketing');
+            $table->dropColumn('last_login_at');
+        });
+    }
+};
---
+++ database/migrations/2022_07_31_113208_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('order_status_id');
+            $table->foreignId('payment_id');
+            $table->char('uuid', 36);
+            $table->json('products');
+            $table->json('address');
+            $table->double('delivery_fee',8,2)->nullable();
+            $table->double('amount',12,2);
+            $table->timestamp('shipped_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
