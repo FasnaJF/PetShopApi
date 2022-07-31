1062b9e767da2d0eae793e4c7616695f7936b01f
jffasna <fasna@rhino-partners.com>
2022-07-31 19:09:35 +0530

    product realted migrations added

---
+++ database/migrations/2022_07_31_133412_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('title');
+            $table->char('uuid', 36);
+            $table->double('price',12,2);
+            $table->text('description');
+            $table->json('metadata');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
