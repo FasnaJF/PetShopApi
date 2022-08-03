a75f1a7b3ba95f3b3b8342d2d223dcbde1ac08ca
jffasna <fasna@rhino-partners.com>
2022-08-03 12:01:53 +0530

    added file resource

--- app/Http/Resources/FileResource.php
+++ app/Http/Resources/FileResource.php
@@ -5,14 +5,31 @@ namespace App\Http\Resources;
 
 class FileResource extends BaseResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        if (isset($this->uuid)) {
+            return [
+                'uuid' => $this->uuid,
+                'name' => $this->name,
+                'path' => $this->path,
+                'size' => $this->size,
+                'type' => $this->type,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ];
+        }
+        return [
+            'data' => $this->map(function ($data) {
+                return [
+                    'uuid' => $data->uuid,
+                    'name' => $data->name,
+                    'path' => $data->path,
+                    'size' => $data->size,
+                    'type' => $data->type,
+                    'created_at' => $data->created_at,
+                    'updated_at' => $data->updated_at,
+                ];
+            })
+        ];
     }
 }
