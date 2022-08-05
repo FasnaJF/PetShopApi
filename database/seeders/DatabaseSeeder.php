11f5de6166e7aa3173f33ac6b9fbd5578cd6fe89
jffasna <fasna@rhino-partners.com>
2022-08-05 18:09:02 +0530

    final seed file prepared

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\OrderStatus;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,16 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         \App\Models\Category::factory(10)->create();
-//         \App\Models\Brand::factory(10)->create();
-         \App\Models\Order::factory(50)->create();
-//         \App\Models\Product::factory(10)->create();
-//        $this->call([
-//            OrderStatusSeeder::class
-//        ]);
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory(30)->create();
+         \App\Models\Category::factory(20)->create();
+         \App\Models\Brand::factory(20)->create();
+         \App\Models\Payment::factory(20)->create();
+        $this->call([
+            OrderStatusSeeder::class
+        ]);
+         \App\Models\Product::factory(50)->create();
+        \App\Models\Order::factory(50)->create();
+
+        /*create admin*/
+         \App\Models\User::factory()->create([
+             'first_name' => 'admin',
+             'last_name' => 'user',
+             'email' => 'admin@petshop.net',
+             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+             'uuid' => Str::uuid(),
+             'email_verified_at' => now(),
+             'remember_token' => Str::random(10),
+             'avatar' => Str::uuid(),
+             'address' => fake()->address(),
+             'phone_number' => fake()->phoneNumber(),
+             'is_marketing' => 1,
+             'created_at' => now(),
+             'updated_at' => now(),
+         ]);
     }
 }
